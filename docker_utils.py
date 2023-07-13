47e6e57cddb45b22ccf93d1122e3c7ecb796789a
Arthur Nangai <arthur.nangai@gmail.com>
2023-07-13 19:14:14 +0300

    Refactor logic to be clearer

--- docker_utils.py
+++ docker_utils.py
@@ -1,6 +1,16 @@
+import os
 import shlex
 import subprocess
 from enum import Enum
+
+from dotenv import load_dotenv
+from fastapi import status
+from fastapi.responses import JSONResponse
+
+from custom_logging import logger
+
+
+load_dotenv()
 
 
 class DockerCommandEnum(Enum):
@@ -15,10 +25,37 @@ DOCKER_COMMAND_MAPPING = {
 
 
 def run_docker_command(docker_command: DockerCommandEnum) -> subprocess.CompletedProcess:
-    return subprocess.run(
-        shlex.split(DOCKER_COMMAND_MAPPING.get(docker_command)),
-        check=True,
-        capture_output=True,
-        encoding="utf-8",
-        timeout=500,
-    )
+    base_dir = os.getcwd()
+    os.chdir(os.getenv('DOCKER_COMPOSE_PATH'))
+    try:
+        subprocess.run(
+            shlex.split(DOCKER_COMMAND_MAPPING.get(docker_command)),
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            timeout=500,
+        )
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=f'Docker command `{DOCKER_COMMAND_MAPPING.get(docker_command)}` complete.'
+        )
+    except FileNotFoundError as exc:
+        logger.exception(exc)
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content='Failed to execute docker compose down file not found.'
+        )
+    except subprocess.CalledProcessError as exc:
+        logger.exception(exc)
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content='Subproccess failed to execute.'
+        )
+    except subprocess.TimeoutExpired as exc:
+        logger.exception(exc)
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content='Request timed out.'
+        )
+    finally:
+        os.chdir(base_dir)
--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import os
-import subprocess
+from fastapi import FastAPI
 
-from dotenv import load_dotenv
-from fastapi import FastAPI, status
-from fastapi.responses import JSONResponse
-
-from custom_logging import logger
-from docker_utils import run_docker_command, DockerCommandEnum
-
-
-load_dotenv()
+from docker_utils import DockerCommandEnum, run_docker_command
 
 
 app = FastAPI()
@@ -19,36 +10,13 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/docker-up-build")
 def docker_up_build():
-    base_dir = os.getcwd()
-    try:
-        os.chdir(os.getenv('DOCKER_COMPOSE_PATH'))
-        run_docker_command(DockerCommandEnum.DOCKER_UP_BUILD)
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content='Docker compose complete'
-        )
-    except FileNotFoundError as exc:
-        logger.exception(exc)
-        return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content='Failed to execute docker compose down file not found.'
-        )
-    except subprocess.CalledProcessError as exc:
-        logger.exception(exc)
-        return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content='Subproccess failed to execute.'
-        )
-    except subprocess.TimeoutExpired as exc:
-        logger.exception(exc)
-        return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content='Request timed out.'
-        )
-    finally:
-        os.chdir(base_dir)
+    """
+    Runs the docker build command
+    """
+    return run_docker_command(DockerCommandEnum.DOCKER_UP_BUILD)
 
 
 @app.get("/docker-down")
@@ -56,31 +24,4 @@ def docker_down():
     """
     Runs docker compose down
     """
-    base_dir = os.getcwd()
-    try:
-        os.chdir(os.getenv('DOCKER_COMPOSE_PATH'))
-        run_docker_command(DockerCommandEnum.DOCKER_DOWN)
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content='Docker compose complete'
-        )
-    except FileNotFoundError as exc:
-        logger.exception(exc)
-        return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content='Failed to execute docker compose down file not found.'
-        )
-    except subprocess.CalledProcessError as exc:
-        logger.exception(exc)
-        return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content='Subproccess failed to execute.'
-        )
-    except subprocess.TimeoutExpired as exc:
-        logger.exception(exc)
-        return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content='Request timed out.'
-        )
-    finally:
-        os.chdir(base_dir)
+    return run_docker_command(DockerCommandEnum.DOCKER_DOWN)
